b4b3bca846d89b96f20ecccefd969616e3c9ee09
ukirsche <ukirschenmann@gmx.de>
2018-06-21 11:21:36 +0200

    Issue #1: Alle Character TextFelder mit Charakter-Werten besetzen 1

--- Scripts/MidgardCharacterSheetManager.cs
+++ Scripts/MidgardCharacterSheetManager.cs
@@ -10,7 +10,31 @@ public class MidgardCharacterSheetManager : MonoBehaviour {
 	abwehrbonus,schadensbonus,zauberbonus,angriffsbonus, sehen, hören, riechen, schmecken, tasten, sechstersinn, sonst, sonstUeberschrift;
 
 
-	//
-	public void SetCharacterValues(){
+	MidgardCharakter mCharacter;
+	MidgardCharacterHelper characterHelper;
+
+	// Use this for initialization
+	void Start () {
+		Toolbox globalVars = Toolbox.Instance;
+		mCharacter = globalVars.mCharacter;
+		characterHelper = globalVars.mCharacterHelper;
+
+		SetCharacterValues ();
+	}
+	
+
+	/// <summary>
+	/// Sets the character values für den erzeugten MidgardCharakter
+	/// </summary>
+	void SetCharacterValues(){
+		typ.text = mCharacter.Archetyp.ToString ();
+		stand.text = mCharacter.Schicht.ToString ();
+		gestalt.text = mCharacter.Gestalt;
+		hand.text = mCharacter.hand.ToString ();
+		alter.text = mCharacter.Alter.ToString ();
+		groesse.text = mCharacter.Groesse.ToString ();
+		gewicht.text = mCharacter.Gewicht.ToString ();
+		staerke.text = mCharacter.St.ToString ();
+		konsitution.text = mCharacter.Ko.ToString ();
 	}
 }
