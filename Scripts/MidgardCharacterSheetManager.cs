db28f8d4a15d657a3d3cb155239b7db7d5ccba81
ukirsche <ukirschenmann@gmx.de>
2018-06-21 10:10:31 +0200

    Issue #1: Alle Character Textfelder in Script

--- Scripts/MidgardCharacterSheetManager.cs
+++ Scripts/MidgardCharacterSheetManager.cs
@@ -6,16 +6,11 @@ using System.Collections.Generic;
 public class MidgardCharacterSheetManager : MonoBehaviour {
 
 	public Text typ, stand, beruf, gestalt, hand, alter, groesse, gewicht, staerke, konsitution, aussehen, selbstbeherrschung, geschicklichkeit, intelligenz, 
-	ausstrahlung, willenskraft, gewandtheit, zaubertalent, bewegungsweite;
+	ausstrahlung, willenskraft, gewandtheit, zaubertalent, bewegungsweite, abwehr, raufen, zaubern, resistenz, ap, lp,ausdauerbonus,
+	abwehrbonus,schadensbonus,zauberbonus,angriffsbonus, sehen, hören, riechen, schmecken, tasten, sechstersinn, sonst, sonstUeberschrift;
 
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	//
+	public void SetCharacterValues(){
 	}
 }
