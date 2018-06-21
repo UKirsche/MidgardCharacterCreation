4309a37afa3f45269a1cf02cb1064bc332750f0e
ukirsche <ukirschenmann@gmx.de>
2018-06-21 13:45:43 +0200

    Issue #1: Methode SetzeSinne

--- Scripts/MidgardCharacterSheetManager.cs
+++ Scripts/MidgardCharacterSheetManager.cs
@@ -7,7 +7,7 @@ public class MidgardCharacterSheetManager : MonoBehaviour {
 
 	public Text typ, stand, beruf, gestalt, hand, alter, groesse, gewicht, staerke, konsitution, aussehen, selbstbeherrschung, geschicklichkeit, intelligenz, 
 	ausstrahlung, willenskraft, gewandtheit, zaubertalent, bewegungsweite, abwehr, raufen, zaubern, resPsy, resPhy, resPhk, ap, lp,ausdauerbonus,
-	abwehrbonus,schadensbonus,zauberbonus,angriffsbonus, sehen, hören, riechen, schmecken, tasten, sechstersinn, sonst, sonstUeberschrift;
+	abwehrbonus,schadensbonus,zauberbonus,angriffsbonus, sehen, hoeren, riechen, schmecken, tasten, sechstersinn, sonst, sonstUeberschrift;
 
 
 	MidgardCharakter mCharacter;
@@ -31,7 +31,6 @@ public class MidgardCharacterSheetManager : MonoBehaviour {
 		SetBasiseigenschaften ();
 		SetKampfWerte ();
 		SetBoni ();
-
 	}
 
 	void SetDemoskopieWerte ()
@@ -81,5 +80,28 @@ public class MidgardCharacterSheetManager : MonoBehaviour {
 		schadensbonus.text = mCharacter.SchB.ToString ();
 		zauberbonus.text = mCharacter.ZauB.ToString ();
 		angriffsbonus.text = mCharacter.AnB.ToString ();
+	}
+
+
+	void SetSinne(){
+		List<Sinn> sinne = mCharacter.listSinne;
+		foreach (var sinn in sinne) {
+			if (sinn.name == "Sehen") {
+				sehen.text = sinn.value.ToString();
+			} else if (sinn.name == "Hören") {
+				hoeren.text = sinn.value.ToString();
+			} else if (sinn.name == "Riechen") {
+				riechen.text = sinn.value.ToString();
+			} else if (sinn.name == "Schmecken") {
+				schmecken.text = sinn.value.ToString ();
+			} else if (sinn.name == "Tasten") {
+				tasten.text = sinn.value.ToString();
+			} else if (sinn.name == "Sechster Sinn") {
+				sechstersinn.text = sinn.value.ToString();
+			} else {
+				sonstUeberschrift.text = sinn.name;
+				sonst.text = sinn.value.ToString ();
+			}
+		}
 	}
 }
