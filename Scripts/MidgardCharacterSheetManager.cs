4f2f7e5aec38f66a6172fbc482b313a2c90967a6
ukirsche <ukirschenmann@gmx.de>
2018-06-21 09:51:52 +0200

    Issue #1: Script für Textanzeige eingestellt

---
+++ Scripts/MidgardCharacterSheetManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MidgardCharacterSheetManager : MonoBehaviour {
+
+	public Text typ, stand, beruf, gestalt, hand, alter, groesse, gewicht, staerke, konsitution, aussehen, selbstbeherrschung, geschicklichkeit, intelligenz, 
+	ausstrahlung, willenskraft, gewandtheit, zaubertalent, bewegungsweite;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
