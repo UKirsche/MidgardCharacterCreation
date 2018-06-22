c13a5be48059ee154d24a2e7951519f543b3afec
ukirsche <ukirschenmann@gmx.de>
2018-06-22 10:11:22 +0200

    Refactoring LernplanInventory Waffen

--- Scripts/LernPlanInventoryWaffen.cs
+++ Scripts/LernPlanInventoryWaffen.cs
@@ -2,38 +2,25 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class LernPlanInventoryWaffen : MonoBehaviour {
-
-	//Verweis auf zu füllendes Panel
-	public LernPlanInventoryDisplay inventoryDisplayPrefab;
-	public Transform displayParent;
-
-	private const string panelName = "WaffenPanel";
+public class LernPlanInventoryWaffen : FillInventory {
 
 
 	// Use this for initialization
-	void Start () {
-
-		//Fülle den Mist auf
-		FillPanelWaffen();
-
+	override void Start () {
+		panelName = "WaffenPanel";
+		base.Start();
 	}
 
 	/// <summary>
 	/// Fills the panel fachkenntnisse. 
 	/// </summary>
-	private void FillPanelWaffen()
+	public override void FillPanel()
 	{
 		Toolbox globalVars = Toolbox.Instance;
 		LernPlanHelper lernHelper = globalVars.lernHelper;
 
 		//Prepare listItems 
 		List<InventoryItem> listItems = lernHelper.GetWaffenfertigkeitItems();
-
-		//Fachkenntnisse werden entsprechend der Lernpunkte aufgelistet
-		inventoryDisplayPrefab = (LernPlanInventoryDisplay)Instantiate (inventoryDisplayPrefab);
-		inventoryDisplayPrefab.name = panelName;
-		inventoryDisplayPrefab.transform.SetParent (displayParent, false);
-		inventoryDisplayPrefab.FillItemDisplay (listItems);
+		ConfigurePrefab (listItems);
 	}
 }
