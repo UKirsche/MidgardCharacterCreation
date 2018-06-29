0890659242fa9c82824e88cbf189f44785446b83
ukirsche <ukirschenmann@gmx.de>
2018-06-29 17:02:04 +0200

    Issue #9: Lösung 1 nicht optimal

--- Scripts/HandleBeruf.cs
+++ Scripts/HandleBeruf.cs
@@ -90,6 +90,6 @@ public class HandleBeruf : MonoBehaviour {
 	{
 		InventoryItemDisplay itemToDisplay = (InventoryItemDisplay)Instantiate (itemDisplayPrefab);
 		itemToDisplay.transform.SetParent (berufPanelDisplay, false);
-		itemToDisplay.SetDisplayValuesCost (item);
+		itemToDisplay.SetDisplayValuesWert (item);
 	}
 }
