4481091959f5a3bb3480ce12d24a329a7bbb9e2b
ukirsche <ukirschenmann@gmx.de>
2018-07-02 14:08:39 +0200

    Issue #2: Zauber als eigenes Panel

---
+++ Scripts/SheetFillZauberInventory.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SheetFillZauberInventory : FillInventory<SheetInventoryDisplay> {
+
+	// Use this for initialization
+	public override void Start () {
+		panelName = "SheetZauber";
+		base.Start ();
+	}
+
+	/// <summary>
+	/// Fills the panel fachkenntnisse. 
+	/// </summary>
+	public override void FillPanel()
+	{
+		Toolbox globalVars = Toolbox.Instance;
+		MidgardCharakter mCharacter = globalVars.mCharacter;
+
+		//Prepare listItems 
+		List<InventoryItem> listItems = mCharacter.zauberFormeln;
+		listItems.AddRange (mCharacter.zauberLieder);
+		listItems.AddRange (mCharacter.zauberSalze);
+		ConfigurePrefab (listItems);
+	}
+}
