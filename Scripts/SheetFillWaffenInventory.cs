2d2b846c83992c6ce274f085fdd1949c000f74c6
ukirsche <ukirschenmann@gmx.de>
2018-07-02 15:14:12 +0200

    Issue #2: fertig, design aber noch...

---
+++ Scripts/SheetFillWaffenInventory.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SheetFillWaffenInventory : FillInventory<SheetInventoryDisplay> {
+
+	// Use this for initialization
+	public override void Start () {
+		panelName = "SheetWaffen";
+		base.Start ();
+	}
+
+	/// <summary>
+	/// Fills the panel fachkenntnisse. 
+	/// </summary>
+	public override void FillPanel()
+	{
+		Toolbox globalVars = Toolbox.Instance;
+		MidgardCharakter mCharacter = globalVars.mCharacter;
+
+		//Prepare listItems 
+		List<InventoryItem> listItems = mCharacter.waffenFertigkeiten;
+		ConfigurePrefab (listItems);
+	}
+}
